95e3fbcb5e68dc57eac3b73d2acdd9ee964ca996
SidamAvinash <106227685+SidamAvinash@users.noreply.github.com>
2022-05-25 12:46:09 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("you are enter arrays is :");
+        for(int i=0;i<n;i++){
+            System.out.print( arr[i]+" ");
+        }
+        System.out.println(" \n Revrse array is:");
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
